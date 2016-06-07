af339c1143e09e50f358ba4e290a97fa1d9cd1dd
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2016-06-07 00:12:22 -0700

    Some dev changes.

--- src/webpack/webpack-isomorphic-tools-configuration.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  assets: {
-    styles: {
-      extensions: [ 'css' ]
-    }
-  },
-  webpack_assets_file_path: '../../webpack-assets.json',
-  webpack_stats_file_path: '../../webpack-stats.json'
-}
--- src/webpack/webpack.config.ts
+++ src/webpack/webpack.config.ts
@@ -5,7 +5,7 @@ export default {
   context: path.resolve(__dirname, '../../src'),
   entry: './index.tsx',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, '../../public'),
     filename: 'app.bundle.js'
   },
   resolve: {
